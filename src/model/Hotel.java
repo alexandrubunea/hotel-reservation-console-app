d53ecf0e27fab93bbe76adc4c5ae10596c3413d3
Bunea Alexandru <alexandrubunea03@gmail.com>
2024-05-15 18:03:48 +0300

    Created hotel class.

---
+++ src/model/Hotel.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.util.ArrayList;
+
+/**
+    * This class represents a hotel as an object.
+*/
+public class Hotel {
+    private int id;
+    private String name;
+    private ArrayList<Room> rooms;
+
+    public Hotel() {
+
+    }
+}
