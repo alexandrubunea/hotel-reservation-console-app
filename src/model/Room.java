ed5cacf66ba7d592ff622284f34a9203e56bf6ed
Bunea Alexandru <alexandrubunea03@gmail.com>
2024-05-15 19:52:49 +0300

    Refactoring

--- src/model/Room.java
+++ src/model/Room.java
@@ -3,21 +3,21 @@ package model;
     * This class represents a hotel's room as an object.
 */
 public class Room {
-    private final int roomNumber;
-    private final String roomType;
+    private final int room_number;
+    private final String room_type;
     private final int price;
 
-    public Room(int roomNumber, String roomType, int price) {
-        this.roomNumber = roomNumber;
-        this.roomType = roomType;
+    public Room(int room_number, String room_type, int price) {
+        this.room_number = room_number;
+        this.room_type = room_type;
         this.price = price;
     }
 
     public int getRoomNumber() {
-        return roomNumber;
+        return room_number;
     }
     public String getRoomType() {
-        return roomType;
+        return room_type;
     }
     public int getPrice() {
         return price;
